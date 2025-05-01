88bf160611fb7789d6b4ec122a5d3a9b503087eb
Daniels4624P <danielfelipesaenzbaquero@gmail.com>
2025-05-01 12:45:10 -0500

    agrego namespaces, salas

--- src/index.js
+++ src/index.js
@@ -15,7 +15,24 @@ app.get('/', (req, res) => {
     return res.sendFile(__dirname + "/views/index.html")
 })
 
-io.on('connection', (socket) => {
+const teachers = io.of('teachers')
+const students = io.of('students')
+
+teachers.on('connection', (socket) => {
+    console.log(socket.id + ' se ha conectado a la sala de profes')
+    socket.on('sendMessage', (data) => {
+        teachers.emit('message', data)
+    })
+})
+
+students.on('connection', (socket) => {
+    console.log(socket.id + ' se ha conectado a la sala de estudiantes')
+    socket.on('sendMessage', (data) => {
+        students.emit('message', data)
+    })
+})
+
+// io.on('connection', (socket) => {
     
     /* console.log('Clientes conectados:', io.engine.clientsCount)
    console.log("ID del socket conectado:", socket.id)
@@ -29,7 +46,7 @@ io.on('connection', (socket) => {
     }) */
 
     // EMISION BASICA
-    socketsOnline.push(socket.id)
+    /* socketsOnline.push(socket.id)
 
     socket.emit('welcome', 'Ahora estas conectado.')
 
@@ -60,7 +77,42 @@ io.on('connection', (socket) => {
 
     setTimeout(() => {
         socket.emit('off', 'Se apaga el evento que pongamos')
-    }, 3000)
-})
+    }, 3000) */
+
+    // socket.on('circlePosition', (positions) => {
+    //     socket.broadcast.emit('moveCircle', positions) // broadcast emite un mensaje para todos excepto para ti en comparacion con io.emit que emite para todos un mensaje
+    // })
+
+    /* socket.connectedRoom = ""
+
+    socket.on('connect to room', (room) => {
+
+        socket.leave(socket.connectedRoom)
+
+        switch (room) {
+            case "room1":
+                socket.join('room1');
+                socket.connectedRoom = 'room1'
+                break
+            case "room2":
+                socket.join('room2');
+                socket.connectedRoom = 'room2'
+                break
+            case "room3":
+                socket.join('room3');
+                socket.connectedRoom = 'room3'
+                break
+        }
+    })
+
+    socket.on('message', (message) => {
+        const room = socket.connectedRoom
+
+        io.to(room).emit('send message', {
+            message,
+            room
+        })
+    }) */  
+// })
 
 httpServer.listen(3000)
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,4 +1,4 @@
-const socket = io()
+// const socket = io()
 
 /* const checkSocketStatus = () => {
     console.log('Estado del socket:', socket.connected)
@@ -26,7 +26,7 @@ socket.io.on('reconnect', () => {
     console.log('Me he vuelto a conectar')
 }) */
 
-socket.on('welcome', (data) => {
+/* socket.on('welcome', (data) => {
     const text = document.querySelector('#text')
     text.textContent = data
 })
@@ -66,4 +66,102 @@ socket.on('off', listener)
 
 setTimeout(() => {
     socket.off('off', listener)
-}, 2000)
+}, 2000) */
+
+/* const circle = document.querySelector('#circle')
+
+const dragCircle = (position) => {
+    circle.style.top = position.top
+    circle.style.left = position.left
+}
+
+const drag = (e) => {
+    const position = {
+        top: e.clientY + "px",
+        left: e.clientX + "px"
+    }
+
+    dragCircle(position)
+    socket.emit('circlePosition', position)
+}
+
+document.addEventListener('mousedown', (event) => {
+    document.addEventListener('mousemove', drag)
+})
+
+document.addEventListener('mouseup', (e) => {
+    document.removeEventListener('mousemove', drag)
+})
+
+socket.on('moveCircle', (position) => {
+    dragCircle(position)
+}) */
+
+/* const connectRoom1 = document.querySelector('#connectRoom1')
+const connectRoom2 = document.querySelector('#connectRoom2')
+const connectRoom3 = document.querySelector('#connectRoom3')
+
+connectRoom1.addEventListener('click', () => {
+    socket.emit('connect to room', 'room1')
+})
+
+connectRoom2.addEventListener('click', () => {
+    socket.emit('connect to room', 'room2')
+})
+
+connectRoom3.addEventListener('click', () => {
+    socket.emit('connect to room', 'room3')
+})
+
+const sendMessage = document.querySelector('#sendMessage')
+
+sendMessage.addEventListener('click', () => {
+    const message = prompt('Escribe tu mensaje')
+    socket.emit('message', message)
+})
+
+socket.on('send message', (data) => {
+    const { room, message } = data
+    const li = document.createElement('li')
+    li.textContent = message
+
+    document.querySelector(`#${room}`).append(li)
+}) */
+
+const user = prompt('Escribe tu usuario');
+
+const profes = ['Retax', 'juandc', 'GNDX'];
+
+let socketNamespace, group
+
+const chat = document.querySelector('#chat')
+const namespace = document.querySelector('#namespace')
+
+if (profes.includes(user)) {
+    socketNamespace = io('/teachers')
+    group = 'teachers'
+} else {
+    socketNamespace = io('/students')
+    group = 'students'
+}
+
+socketNamespace.on('connect', () => {
+    namespace.textContent = group
+})
+
+const sendMessage = document.querySelector('#sendMessage')
+
+sendMessage.addEventListener('click', () => {
+    const message = prompt('Escribe tu mensaje')
+    socketNamespace.emit('sendMessage', {
+        message,
+        user
+    })
+})
+
+socketNamespace.on('message', (data) => {
+    const { user, message } = data 
+    const li = document.createElement('li')
+    li.textContent = `${user}: ${message}`;
+    chat.append(li)
+})
